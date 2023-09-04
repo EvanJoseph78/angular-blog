067069f5718854e6a4c8aac6497da09bf4f854d0
Evan Joseph <evandromariano49@gmail.com>
2023-09-03 23:30:52 -0300

    feat: rotas dinamicas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo: 'evan',
+    title: 'Novo anime boruto shippudden',
+    description: 'Boruto tem o drip!',
+  },
+  {
+    id: 2,
+    photo: 'joseph',
+    title: 'Hakuna matata',
+    description: 'É lindo dizer!',
+  },
+];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent {
-
+  @Input()
+  photoCover: string = 'https://tm.ibxk.com.br/2021/11/11/11185008794612.jpg?ims=352x208';
+  @Input()
+  contentCover: string = 'Minha notícia';
+  @Input()
+  contentDescription: string = 'Evan Joseph';
 }
