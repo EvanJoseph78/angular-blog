cdec55730d5fe05fe48a8879fbb0887fef4a8807
Evan Joseph <evandromariano49@gmail.com>
2023-09-03 22:41:30 -0300

    feat: componentes desacoplados

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
-  
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string =
+    'Minato Namikaze, também conhecido como "Quarto Hokage," é um personagem da série de anime e mangá "Naruto." Ele é amplamente reconhecido como um dos ninjas mais poderosos e habilidosos do universo Naruto, e isso sedeve a várias razões:';
+
+  constructor() {}
 }
